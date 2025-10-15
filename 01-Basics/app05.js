03bacd9511bcdb4870fd765503038e311d0b0443
Nattanahel Alberto Chaves Moya <nattanahel.chaves@3pillarglobal.com>
2025-10-15 15:00:29 -0600

    Adding code for the second section

---
+++ 01-Basics/app05.js
@@ -0,0 +1,45 @@
+const http= require('http');
+const fs=require('fs');
+
+const server=http.createServer((req,res)=>{
+    if(req.url==='/')
+    {
+        res.setHeader('Content-Type','text/html');
+        res.write('<html>')
+        res.write('<head><title>Node.JS Server is running</title></head>')
+        res.write('<body><h1>Enter your name</h1>')
+        res.write('<form action="/message" method="POST"><input type="text" name="message"/><button type="submit">Send</button></form></body>')
+        res.write('</html>')
+        return res.end();
+    }
+    if (req.url==='/message' && req.method==='POST')
+    {
+        const body=[];
+        //Start to receive the data.
+        req.on('data', (chunckOfData)=>{
+            console.log(chunckOfData);
+            body.push(chunckOfData);
+        });
+        //When all the data is received, save the data in a file.
+        req.on('end',()=>{
+            const parsedBody=Buffer.concat(body).toString();
+            console.log(parsedBody);
+            const message=parsedBody.split('=')[1];
+            //only use writeFileSync is the size of the file is very small.
+            fs.writeFileSync('message.txt', message);
+            res.statusCode=302;
+            res.setHeader('Location','/');
+            return res.end();
+        });
+        
+
+    }
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>')
+    res.write('<head><title>Node.JS Server is running</title></head>')
+    res.write('<body><h1>Hello world from node.js server</h1></body>')
+    res.write('</html')
+    res.end();
+});
+
+server.listen(3000);
